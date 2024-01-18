757f45b7bf77380fd9a7553406c30afb84a8c30b
hgmorris <113636731+hgmorris@users.noreply.github.com>
2024-01-18 14:03:03 -0700

    edit

---
+++ controllers/professional.js
@@ -0,0 +1,11 @@
+const mongodb = require('./db/connect');
+
+const getData = async (req, res, next) => {
+  const result = await mongodb.getDb().db().collection('user').find();
+  result.toArray().then((lists) => {
+    res.setHeader('Content-Type', 'application/json');
+    res.status(200).json(lists[0]); // we just need the first one (the only one)
+  });
+};
+
+module.exports = { getData };
